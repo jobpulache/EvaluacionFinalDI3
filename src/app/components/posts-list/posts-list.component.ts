0ee992af7a9cfa83186e332b9b7e046e6b3e6130
Job Pulache <jobpulachecarrenojob837@gmail.com>
2024-12-20 14:41:49 -0500

    ending the four exercise

--- src/app/components/posts-list/posts-list.component.ts
+++ src/app/components/posts-list/posts-list.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { PostsService } from '../../services/posts.service';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
   imports: [
+    CommonModule,
     MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
     NavbarComponent
   ],
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent {}
+export class PostsListComponent implements OnInit {
+  posts: Post[] = [];
+  displayedColumns: string[] = ['id', 'title', 'body'];
+  isLoading = true;
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.postsService.getPosts().subscribe({
+      next: (data) => {
+        this.posts = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching posts:', error);
+        this.isLoading = false;
+      }
+    });
+  }
+}
